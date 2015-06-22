9270b2b16e8284809b1570d589c556aef9ba61ec
Umar Toseef <umar.toseef@eict.de>
2015-06-22 15:06:50 +0200

    Fixed issue with keyboard input for CBAS authority name in gen-cert script to avoid Travis build timeout.

--- test/creds/gen-certs.sh
+++ test/creds/gen-certs.sh
@@ -1,5 +1,10 @@
 mkdir tmp/ deploy/trusted/ deploy/trusted/certs/ deploy/trusted/cert_keys/ deploy/trusted/crl/
+if [ -z "$1" ]
+then
 python src/vendor/geni_trust/gen-certs.py tmp/
+else
+python src/vendor/geni_trust/gen-certs.py tmp/ --authority $1
+fi
 cp tmp/ca-cert.pem deploy/trusted/certs/
 cp tmp/ca-key.pem deploy/trusted/cert_keys/
 cp tmp/sa-cert.pem deploy/trusted/certs/
